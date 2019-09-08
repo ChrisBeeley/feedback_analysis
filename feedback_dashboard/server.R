2e63a4850b43f37cb4156c3d45d0f7f9c7f156f2
Chris Beeley <chris.beeley@gmail.com>
2019-09-08 13:48:46 +0100

    This did not really work

--- feedback_dashboard/server.R
+++ feedback_dashboard/server.R
@@ -3,11 +3,10 @@
 
 # some code borrowed from here https://datascience-enthusiast.com/R/R_shiny_Tableau_treemap.html
 
-library(gridBase)
 library(feather)
 library(tidyverse)
-library(treemap)
 library(lubridate)
+library(treemapify)
 
 main_data <- read_feather("vader.feather") %>% 
     mutate(Date = floor_date(as.Date(Date))) %>% 
@@ -24,78 +23,26 @@ topics <- read_feather("topics.feather") %>%
 main_data <- main_data %>% 
     left_join(topics, by = c("topic" = "topic_number"))
 
-frequencies <- main_data %>% 
-    group_by(words, pos_neg) %>% 
-    count()
-
-### Handle cliks on a treemap
-tmLocate <-
-    function(coor, tmSave) {
-        tm <- tmSave$tm
-        
-        # retrieve selected rectangle
-        rectInd <- which(tm$x0 < coor[1] &
-                             (tm$x0 + tm$w) > coor[1] &
-                             tm$y0 < coor[2] &
-                             (tm$y0 + tm$h) > coor[2])
-        
-        return(tm[rectInd[1], ])
-        
-    }
-
 # shiny server----
 
 function(input, output) {
     
-    # topic treemap----
+    # data
+    
+    treemapData <- reactive({
+        
+        frequencies <- main_data %>% 
+            group_by(words, pos_neg) %>% 
+            count()
+    })
     
     output$treeMap <- renderPlot({
         
-        vps <- baseViewports()
+        ggplot(frequencies, aes(area = n,
+                       fill = words, subgroup = pos_neg)) + geom_treemap()
         
-        .tm <<- treemap(frequencies,
-                        index=c("words", "pos_neg"),
-                        vSize="n",
-                        type="index",
-                        align.labels = list(c("centre","centre"),c("left","top"))
-        )
-    })
-    
-    treemap_clicked_country <- reactiveValues(
-        center = NULL,
-        for_condition = NULL
-    )
-    
-    # Handle clicks on treemap by country
-    observeEvent(input$click_treemap_country, {
-        x <- input$click_treemap_country$x
-        y <- input$click_treemap_country$y
-        treemap_clicked_country$center <- c(x,y)
-        
-        if(is.null(treemap_clicked_country$for_condition)){
-            treemap_clicked_country$for_condition=c(x,y)
-        }
-        else{treemap_clicked_country$for_condition=NULL}
-    })
-    
-    getRecord_population_country <- reactive({
-        x <- treemap_clicked_country$center[1]
-        y <- treemap_clicked_country$center[2]
-        
-        x <- (x - .tm$vpCoorX[1]) / (.tm$vpCoorX[2] - .tm$vpCoorX[1])
-        y <- (y - .tm$vpCoorY[1]) / (.tm$vpCoorY[2] - .tm$vpCoorY[1])
-        
-        
-        l <- tmLocate(list(x=x, y=y), .tm)
-        z=l[, 1:(ncol(l)-5)]
-        
-        if(is.na(z[, 1])){
-            return(NULL)
-        }
-        
-        col = as.character(z[,1])
-        
-        return(col)
+        #, 
+                       # subgroup=YEAR))
     })
     
     output$showReactive <- renderText({
@@ -112,31 +59,9 @@ function(input, output) {
         paste("<p>", comment_selection, "</p>")
     })
     
-    # sentiment tab----
-    
-    beeswarmGraph <- reactive({
+    output$showNearPoints <- renderText({
         
-        sentiment %>% 
-            filter(!is.na(my_compound)) %>% 
-            sample_n(100)
+        nearPoints()
     })
     
-    output$beeswarmComments <- renderPlot({
-        
-        beeswarmGraph() %>% 
-            ggplot(aes(x = Date, y = my_compound)) + 
-            geom_point() + 
-            scale_colour_brewer(palette = "Spectral") + 
-            theme(axis.text.x=element_text(angle = 45)) 
-    })
-    
-    output$beeswarmText <- renderText({
-
-        beeswarm_df <- nearPoints(beeswarmGraph(), input$beeswarm_click, threshold = 100, maxpoints = 1)
-        
-        validate(need(nrow(beeswarm_df) > 0, "Click a point to see the comment"))
-
-        with(beeswarm_df, paste0(Keep_Improve, " (", Location, ")"))
-
-    })
 }
--- feedback_dashboard/ui.R
+++ feedback_dashboard/ui.R
@@ -2,14 +2,17 @@
 navbarPage(title = "Unsupervised processing of patient feedback data",
            
            tabPanel("Topics",
-               fluidRow(
-                   column(3, htmlOutput("showReactive")),
-                   column(9, plotOutput("treeMap", height="600px",
-                                        click="click_treemap_country")))
+                    fluidRow(
+                      column(3, htmlOutput("showReactive")),
+                      column(9, plotOutput("treeMap", height="600px",
+                                           click="click_treemap")))
            ),
            tabPanel("Sentiment",
                     fluidRow(
-                        column(3, textOutput("beeswarmText")),
-                        column(9, plotOutput("beeswarmComments", click = "beeswarm_click")))
+                      column(3, textOutput("beeswarmText")),
+                      column(9, plotOutput("beeswarmComments", click = "beeswarm_click"))),
+                    fluidRow(
+                      textOutput("showNearPoints")
                     )
+           )
 )
