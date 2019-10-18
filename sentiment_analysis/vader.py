42b92747a90787d53ff353682ee029ac5f81a3f7
Chris Beeley <chris.beeley@gmail.com>
2019-10-18 15:06:42 +0100

    This commits a couple of minor edits to the VADER and topic models, and adds deep learning
    This is the state of the code before the meeting with NUH and LPT on
    15th October 2019. The deep learning achieved 62% accuracy across 17 categories, which is
    okay considering the number of categories and the relatively poor state of the data
    
    Most of the work remains in spreadsheets and documents/ presentation slides.
    
    Work has now begun to prepare a functioning dashboard which combines the methodology within
    this repository in a coherent way

--- sentiment_analysis/vader.py
+++ sentiment_analysis/vader.py
@@ -8,14 +8,20 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-df_all = pd.read_csv('for_gensim.csv')
+# df_all = pd.read_csv('for_gensim.csv')
+
+# leicester DataFrame
+
+df_all = pd.read_csv("/home/chris/Nextcloud/feedback_analysis/leicester.csv", encoding = "latin-1", na_values=[])
 
 # %% analysis
 
 analyser = SentimentIntensityAnalyzer()
 
-my_score = df_all.Keep_Improve.apply(analyser.polarity_scores)
+my_score = df_all.Improve.apply(analyser.polarity_scores)
 
-df_all["my_compound"] = my_score.apply(lambda x: x["compound"])
+df_all["sentiment"] = my_score.apply(lambda x: x["compound"])
 
-df_all.to_feather('vader.feather')
+df_all.to_feather('vader_leicester.feather')
+
+df_all.to_csv(path_or_buf = "leicester_sentiment.csv", index=False)
--- topic_modelling/topic_models.py
+++ topic_modelling/topic_models.py
@@ -8,7 +8,19 @@ import inspect
 
 # %%
 
-feedback = pd.read_csv("/home/chris/Nextcloud/feedback_analysis/for_gensim.csv", encoding = "latin-1", na_values=[])
+# feedback = pd.read_csv("/home/chris/Nextcloud/feedback_analysis/for_gensim.csv", encoding = "latin-1", na_values=[])
+
+feedback = pd.read_csv("/home/chris/Nextcloud/feedback_analysis/leicester.csv", encoding = "UTF-8", na_values=[])
+
+# grep for 8877
+
+feedback_subset = feedback[feedback['Improve'].str.contains("8877")]
+
+feedback_subset
+
+feedback["word_count"] = feedback["Improve"].apply(lambda x: len(x.split()))
+
+feedback = feedback[feedback['word_count'] > 10]
 
 # convert the 2nd column values to a list
 documents = feedback["Improve"].tolist()
@@ -18,7 +30,7 @@ documents = feedback["Improve"].tolist()
 # %% write a document with the topics in
 
 no_top_words = 8
-no_top_documents = 10
+no_top_documents = 20
 
 from docx import Document
 from docx.shared import Inches
@@ -40,6 +52,11 @@ no_topics = 5
 document = Document()
 
 document.add_heading('Topics', 0)
+
+# NMF is able to use tf-idf
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, stop_words='english')
+tfidf = tfidf_vectorizer.fit_transform(documents)
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
 # Run NMF
 nmf_model = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
@@ -71,8 +88,30 @@ no_topics = 10
 
 document = Document()
 
+"""
+
+# df matrix
+
+vec = CountVectorizer().fit(documents)
+bag_of_words = vec.transform(documents)
+sum_words = bag_of_words.sum(axis=0)
+total_words = bag_of_words.sum()
+# I added a bit to divide by the total number of words
+words_freq = [(word, sum_words[0, idx] / total_words)  for word, idx in     vec.vocabulary_.items()]
+words_freq = sorted(words_freq, key = lambda x: x[1], reverse=True)
+words_freq
+
+this is the top 200 most common words
+
+['and', 'the', 'to', 'very', 'was', 'my', 'have', 'with', 'of', 'for', 'good', 'it', 'me', 'is', 'in', 'you', 'staff', 'been', 'has', 'all', 'be', 'service',  'this', 'not',  'are',  'at',  'so',  'help', 'care',  'that',  'on',  'as',  'would',  'were',  'friendly',  'but',  'they',  'more',  'really', 'thank', 'time', 'had', 'could', 'from', 'excellent', 'feel', 'we', 'she', 'well', 'given', 'when', 'helped', 'advice', 'support', 'food', 'her', 'about', 'better', 'what', 'can', 'information', 'made', 'great', 'do', 'after', 'get', 'exercises', 'much', 'always', 'hospital', 'found', 'like', 'nice', 'by', 'lot', 'am', 'everything', 'only', 'if', 'caring', 'us', 'an',  'there', 'professional', 'done', 'happy', 'received', 'up', 'informative', 'people', 'useful', 'lovely', 'think', 'who', 'home', 'how', 'physio', 'name', 'out', 'felt', 'treatment', 'extremely', 'no',  'nurse', 'also', 'will', 'kind', 'your', 'being', 'patient', 'health', 'explained', 'understanding', 'which', 'child', 'needs', 'because', 'enjoyed', 'needed', 'work', 'know', 'team', 'some', 'phone', 'any', 'need', 'experience', 'them', 'way', 'course', 'gave', 'first', 'or', 'questions', 'things', 'recommend', 'our', 'here', 'group', 'too', 'back', 'appointment', 'ward', 'don', 'everyone', 'lots', 'daughter', 'session', 'other', 'see', 'supportive', 'one', 'he', 'now', 'make', 'nurses', 'easy', 'did', 'family', 'person', 'improve', 'long', 'looked', 'nothing', 'problem', 'times', 'sessions', 'understand', 'life', 'their', 'patients', 'enough', 'exercise', 'over', 'weeks', 'waiting', 'pain', 'visit', 'baby', 'son', 'improved', 'different', 'brilliant', 'provided', 'talk', 'knowledgeable', 'children', 'anything', 'myself', 'stay', 'every', 'able', 'doing', 'listened', 'best', 'pleased', 'just', 'go', 'than']
+
+"""
+
+# now I'm going to use a custom stop word list
+
 # NMF is able to use tf-idf
-tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, stop_words='english')
+# tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, stop_words='english')
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, stop_words=['and', 'the', 'to', 'very', 'was', 'my', 'have', 'with', 'of', 'for', 'good', 'it', 'me', 'is', 'in', 'you', 'staff', 'been', 'has', 'all', 'be', 'service',  'this', 'not',  'are',  'at',  'so',  'that',  'on',  'as',  'would',  'were', 'but',  'they',  'more',  'really', 'had', 'could', 'from', 'feel', 'we', 'she', 'well', 'given', 'when', 'her', 'about', 'better', 'what', 'can', 'made', 'great', 'do', 'after', 'get', 'much', 'always', 'found', 'like', 'nice', 'by', 'lot', 'am', 'everything', 'only', 'if', 'us', 'an',  'there', 'done', 'happy', 'received', 'up', 'people', 'useful', 'lovely', 'who', 'how', 'name', 'out', 'felt', 'extremely', 'no',  'also', 'will', 'your', 'being', 'which', 'because', 'know', 'some', 'any', 'them', 'way', 'gave', 'first', 'or', 'things', 'our', 'here', 'group', 'too', 'back', 'don', 'everyone', 'lots', 'other', 'see', 'one', 'he', 'now', 'make', 'easy', 'did', 'nothing', 'their', 'enough', 'over', 'anything', 'myself', 'every', 'able', 'doing', 'best', 'pleased', 'just', 'go', 'than'])
 tfidf = tfidf_vectorizer.fit_transform(documents)
 tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
@@ -85,32 +124,4 @@ display_topics(nmf_H, nmf_W, tfidf_feature_names, documents, no_top_words, no_to
 
 document.save('ten_topics.docx')
 
-# %% so I selected 10 topics. Let's dump the whole thing to R
-
-# this bit works and produces a list of topic headings
-# and in the same order to top words from those headings
-
-topic_heading  = []
-topic_words = []
-
-for topic_idx, topic in enumerate(nmf_H):
-    topic_heading.append("Topic %d:" % (topic_idx))
-    topic_words.append(" ".join([tfidf_feature_names[i]
-                    for i in topic.argsort()[:-no_top_words - 1:-1]]))
-
-# topic_heading
-# topic_words
-
-# %% this next bit is to write which topic every document belongs to
-
-results = np.apply_along_axis(lambda x : np.argmax(x), 1, nmf_W)
-
-feedback["topic"] = results
-
-# save the topic models
-
-feedback.to_feather('topic_modelled.feather')
-
-# now save what they are
-
-pd.DataFrame(topic_words, columns = ["words"]).to_feather("topics.feather")
+# %% so I selected 10 topics. See other code for actual topic model output
